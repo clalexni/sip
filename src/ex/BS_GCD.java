2c710309e929faace4fd94f9260e877409d23b30
Alex Ni <cl.alexni@gmail.com>
2021-12-30 17:12:25 -0600

    bs_gcd

--- src/ex/BS_GCD.java
+++ src/ex/BS_GCD.java
@@ -1,6 +1,7 @@
 package ex;
 
-public class BS {
+public class BS_GCD {
+
   public static <T extends Comparable<? super T>> int bs(T[] orderedList, T target) {
     int left = 0, right = orderedList.length-1;
     while (left <= right) {
@@ -16,9 +17,20 @@ public class BS {
     return -1;
   }
 
+  public static int gcd(int a, int b) {
+    int mod = a%b;
+    while (mod != 0) {
+      a = b;
+      b = mod;
+      mod = a % b;
+    }
+    return b;
+  }
+
   public static void main(String ...args) {
     // NOTE: autoboxing only works for single element
     Integer[] a = {1, 3, 6, 10, 17, 33, 45, 49, 52, 64, 76, 78, 82, 90, 91, 97};
     System.out.println(bs(a, 1));
+    System.out.println(gcd(102, 15));
   }
 }
