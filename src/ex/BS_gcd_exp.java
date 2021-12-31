6390a5ab06b10f7e35e414081abd0e267612d876
Alex Ni <cl.alexni@gmail.com>
2021-12-30 20:29:54 -0600

    bs gcd exp

--- src/ex/BS_gcd_exp.java
+++ src/ex/BS_gcd_exp.java
@@ -1,6 +1,8 @@
 package ex;
 
-public class BS_GCD {
+import javax.swing.plaf.synth.SynthIcon;
+
+public class BS_gcd_exp {
 
   public static <T extends Comparable<? super T>> int bs(T[] orderedList, T target) {
     int left = 0, right = orderedList.length-1;
@@ -27,10 +29,26 @@ public class BS_GCD {
     return b;
   }
 
+  public static long exp(int a, int b) {
+    if (b == 0) {
+      return 1;
+    }
+    if ( b == 1) {
+      return a;
+    }
+
+    if (b % 2 == 0) {
+      return exp(a*a, b/2);
+    } else {
+      return exp(a*a, b/2) * a;
+    }
+  }
+
   public static void main(String ...args) {
     // NOTE: autoboxing only works for single element
     Integer[] a = {1, 3, 6, 10, 17, 33, 45, 49, 52, 64, 76, 78, 82, 90, 91, 97};
     System.out.println(bs(a, 1));
     System.out.println(gcd(102, 15));
+    System.out.println(exp(2, 10));
   }
 }
