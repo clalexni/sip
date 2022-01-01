75197d43bac7e69e9bf22a8c9349fe90bc2c4a0c
Alex Ni <cl.alexni@gmail.com>
2022-01-01 15:47:31 -0600

    ll skeleton

--- src/p1/LL.java
+++ src/p1/LL.java
@@ -1,5 +1,88 @@
 package p1;
 
-public class LL {
-  
+import java.util.Iterator;
+
+public class LL<T> implements Iterable<T>{
+
+  private int size;
+  private int modCount = 0;
+  private Node<T> beginMarker;
+  private Node<T> endMarker;
+
+  /* static nested class Node */
+  private static class Node<T> {
+    private T data;
+    private Node<T> prev;
+    private Node<T> next;
+
+    public Node(T data, Node<T> prev, Node<T> next) {
+      this.data = data;
+      this.prev = prev;
+      this.next = next;
+    }
+  }
+
+  @Override
+  public Iterator<T> iterator() {
+    return new LinkedListIterator();
+  }
+
+  /* constructor */
+  public LL() {
+  }
+
+  // Collection interface
+  public void clear() {
+  }
+
+  public int size() {
+    return this.size;
+  }
+
+  public boolean isEmpty() {
+    return this.size == 0;
+  }
+
+  public boolean add(T x) {
+    return false;
+  }
+
+  // List interface
+  public void add(int idx, T x) {
+  }
+
+  public T get(int idx) {
+    return null;
+  }
+
+  public T set(int idx, T newVal) {
+    return null;
+  }
+
+  public T remove(int idx) {
+    return null;
+  }
+
+  public String toString() {
+    return null;
+  }
+
+  private class LinkedListIterator implements java.util.Iterator<T> {
+
+    @Override
+    public boolean hasNext() {
+      return false;
+    }
+
+    @Override
+    public T next() {
+      return null;
+    }
+    
+  }
+}
+
+class TestLL {
+  public static void main(String[] args) {
+  }
 }
