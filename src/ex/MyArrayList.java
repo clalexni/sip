d050d335263db9a9642c05bea14cb33b4a222d11
Alex Ni <cl.alexni@gmail.com>
2021-12-31 19:38:30 -0600

    al api skeleton

--- src/ex/MyArrayList.java
+++ src/ex/MyArrayList.java
@@ -1,5 +1,76 @@
 package ex;
 
-public class MyArrayList {
-  
+import java.util.Iterator;
+
+public class MyArrayList <T> implements Iterable<T>{
+
+  private static final int DEFAULT_CAPACITY = 10;
+  private T[] items[];
+  private int size;
+
+  public MyArrayList() {
+  }
+
+  @Override
+  public Iterator<T> iterator() {
+    return null;
+  }
+
+  public void ensureCapacity(int newCapacity) {
+  }
+
+  public int size() {
+    return this.size;
+  } 
+
+  public boolean isEmpty() {
+    return size() == 0;
+  }
+
+  public void clear() {
+  }
+
+  public T get(int idx) {
+    return null;
+  }
+
+  public T set(int idx, T newVal) {
+    return null;
+  }
+
+  public boolean add(T x) {
+    return false;
+  }
+
+  public void add(int idx, T x) {
+  }
+
+  public T remove(int idx) {
+    return null;
+  }
+
+  public String toString() {
+    return null;
+  }
 }
+
+class ArrayListIterator<T> implements java.util.Iterator<T> {
+
+  private static final int DEFAULT_CAPACITY = 10;
+  private T items[];
+  private int size;
+
+  @Override
+  public boolean hasNext() {
+    return false;
+  }
+
+  @Override
+  public T next() {
+    return null;
+  }
+  
+  public void remove() {
+  }
+}
+
