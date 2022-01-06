1e0968e3b14d6d2fb9ce95f200ee01532418a5ac
Alex Ni <cl.alexni@gmail.com>
2022-01-05 20:04:51 -0600

    insert and print

--- src/p2/BST.java
+++ src/p2/BST.java
@@ -1,4 +1,16 @@
 package p2;
+/*
+insert
+remove
+findMin
+findMax
+contains
+makeEmpty
+isEmpty
+printTree
+*/
+
+import javax.xml.transform.Source;
 
 public class BST<T extends Comparable <? super T>>{
 
@@ -25,6 +37,21 @@ public class BST<T extends Comparable <? super T>>{
   }
 
   public void insert(T x) {
+    root = insert(x, root);
+  }
+  
+  private Node<T> insert(T x, Node<T> node) {
+    if (node == null) { 
+      return new Node<>(x);
+    }
+    int compareResult = x.compareTo(node.data);
+    if (compareResult > 0) { // x > node.data
+      node.right = insert(x, node.right);
+    } else if (compareResult < 0) { // x < node.data
+      node.left = insert(x, node.left);
+    } else {  
+    }
+    return node;
   }
 
   public void remove(T x) {
@@ -51,8 +78,27 @@ public class BST<T extends Comparable <? super T>>{
   }
 
   public void printTree() {
+    if (isEmpty()) {
+      System.out.println("Empty Tree");
+    } else {
+      printTree(this.root);
+    }
+  }
+
+  // print tree in sorted order, which is inorder traversal
+  private void printTree(Node<T> node) {
+    if (node != null) {
+      printTree(node.left);
+      System.out.println(node.data);
+      printTree(node.right);
+    }
   }
 
   public static void main(String[] args) {
+    BST<Integer> bst = new BST<>();
+    bst.insert(2);
+    bst.insert(1);
+    bst.insert(3);
+    bst.printTree();
   }
 }
