53077a95c3a117e2c724df810d20d314a5963d08
Alex Ni <cl.alexni@gmail.com>
2022-01-05 19:29:38 -0600

    bst skeleton before p2

--- src/p2/BST.java
+++ src/p2/BST.java
@@ -1,5 +1,58 @@
 package p2;
 
-public class BST {
-  
+public class BST<T extends Comparable <? super T>>{
+
+  private Node<T> root;  
+
+  private static class Node <T> {
+    public T data;
+    public Node<T> left;
+    public Node<T> right;
+
+    public Node(T data) {
+      this.data = data;
+    }
+
+    public Node(T data, Node<T> left, Node<T> right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public BST() {
+    root = null;
+  }
+
+  public void insert(T x) {
+  }
+
+  public void remove(T x) {
+  }
+
+  public T findMin() {
+    return null;
+  }
+
+  public T findMax() {
+    return null;
+  }
+
+  public boolean contains(T x) {
+    return false;
+  }
+
+  public void makeEmpty() {
+    root = null;
+  }
+
+  public boolean isEmpty() {
+    return root == null;
+  }
+
+  public void printTree() {
+  }
+
+  public static void main(String[] args) {
+  }
 }
