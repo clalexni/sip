6d3b687600005825d90c1ccd00c9af471befc24a
Alex Ni <cl.alexni@gmail.com>
2022-01-08 16:42:41 -0600

    LL single rotation

--- src/ex/AvlTree.java
+++ src/ex/AvlTree.java
@@ -18,6 +18,7 @@ package ex;
 public class AvlTree<T extends Comparable<? super T>> {
 
   private Node<T> root;
+  private static final int ALLOWED_IMBALANCE = 1;
 
   private static class Node<T> {
     public T data;
@@ -35,7 +36,39 @@ public class AvlTree<T extends Comparable<? super T>> {
   }
 
   private Node<T> balance(Node<T> node) {
-    return null;
+    if (node == null) {
+      return null;
+    }
+    if (height(node.left) - height(node.right) > ALLOWED_IMBALANCE) { 
+      if (height(node.left.left) > height(node.left.right)) {
+        // LL single rotation
+        node = LLSingleRotation(node);
+      } else if (height(node.left.right) > height(node.left.left)) {
+        // LR double rotation
+      } else {
+        System.out.println("how did you end up here?");
+      }
+    } else if (height(node.right) - height(node.left) > ALLOWED_IMBALANCE) {
+      if (height(node.right.right) > height(node.right.left)) {
+        // RR single rotation
+      } else if (height(node.right.right) < height(node.right.left)) {
+        // RL double rotation
+      } else {
+        System.out.println("how did you end up here 2?");
+      }
+    }
+    node.height = Math.max(height(node.left), height(node.right)) + 1;
+    return node;
+  }
+
+  private Node<T> LLSingleRotation(Node<T> k2) {
+    Node<T> k1 = k2.left;
+    k2.left = k1.right;
+    k1.right = k2;
+    // only k1 and k2's heights changed
+    k2.height = Math.max(height(k2.left), height(k2.right)) + 1;
+    k1.height = Math.max(height(k1.left), height(k1.right)) + 1;
+    return k1;
   }
 
   private int height(Node<T> node) {
@@ -58,7 +91,7 @@ public class AvlTree<T extends Comparable<? super T>> {
     } else {
       ;
     }
-    return node;
+    return balance(node);
   }
 
 
