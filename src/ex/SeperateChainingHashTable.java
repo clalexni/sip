fe1a90a69fa590ae6353596631c5609830d274ce
Alex Ni <cl.alexni@gmail.com>
2022-01-13 17:17:31 -0600

    sep chain insert

--- src/ex/SeperateChainingHashTable.java
+++ src/ex/SeperateChainingHashTable.java
@@ -17,22 +17,55 @@ public class SeperateChainingHashTable<T> {
     this(DEFAULT_TABLE_SIZE);
   }
   public SeperateChainingHashTable(int size) {
-    arr = new LinkedList[size];
+    arr = new LinkedList[nextPrime(size)];
     for (int i = 0; i < arr.length; i++) {
       arr[i] = new LinkedList<>();
     }
   }
 
   public boolean insert(T x) {
+    // find hash or bucket id
+    // see if linklist contains x
+    List<T> list = arr[myHash(x)];
+    if (!list.contains(x)) { // use equals in background
+      list.add(x);
+      return true;
+    }
     return false;
   }
 
-  // private int findPos(T x) {
+  private int myHash(T x) {
+    int hashVal = x.hashCode() % arr.length;
+    if (hashVal < 0) {
+      hashVal += arr.length;
+    }
+    return hashVal;
+  }
 
-  // }
 
-  // private int nextPimre() {
-  // }
+  private int nextPrime(int num) {
+    if (num % 2== 0) {
+      num++;
+    }
 
+    while (!isPrime(num)) {
+      num += 2;
+    }
+    return num;
+  }
+
+  private boolean isPrime(int num) {
+    for (int i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  public static void main(String[] args) {
+    SeperateChainingHashTable<Integer> ht = new SeperateChainingHashTable<>();
+    System.out.println(ht.insert(1));
+    System.out.println(ht.insert(1));
+  }
 }
 
