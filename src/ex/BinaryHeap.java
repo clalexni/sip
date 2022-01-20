13ee6c3dcea2bf1a28b70f3e3f2d5d776cb5fd0b
Alex Ni <cl.alexni@gmail.com>
2022-01-19 19:53:20 -0600

    binary heap skeleton

---
+++ src/ex/BinaryHeap.java
@@ -0,0 +1,44 @@
+// ******************PUBLIC OPERATIONS*********************
+// void insert( x )       --> Insert x
+// Comparable deleteMin( )--> Return and remove smallest item
+// Comparable findMin( )  --> Return smallest item
+// boolean isEmpty( )     --> Return true if empty; else false
+// void makeEmpty( )      --> Remove all items
+package ex;
+
+public class BinaryHeap <T extends Comparable<? super T>> {
+  private static final int DEFAULT_CAPACITY = 10;
+  private int size;
+  private T[] arr;
+
+  public BinaryHeap() {
+  }
+
+  public BinaryHeap(int capacity) {
+  }
+
+  public BinaryHeap(T[] arr) {
+
+  }
+
+  public void insert(T x) {
+  }
+
+  public T findMin() {
+    return null;
+  }
+
+  public T deleteMin() {
+    return null;
+  }
+
+  public boolean isEmpty() {
+    return false;
+  }
+
+  public void makeEmpty() {
+  }
+
+  public static void main(String[] args) {
+  }
+}
