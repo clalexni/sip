dea2f0039873ce6ba68d18c5a942a03400863531
Alex Ni <cl.alexni@gmail.com>
2022-01-20 14:33:51 -0600

    insert, findMin

--- src/ex/BinaryHeap.java
+++ src/ex/BinaryHeap.java
@@ -12,20 +12,48 @@ public class BinaryHeap <T extends Comparable<? super T>> {
   private T[] arr;
 
   public BinaryHeap() {
+    this(DEFAULT_CAPACITY);
   }
 
   public BinaryHeap(int capacity) {
+    size = 0;
+    arr = (T[])new Comparable[capacity + 1];
   }
 
   public BinaryHeap(T[] arr) {
+  }
 
+  private void buildHeap() {
   }
 
   public void insert(T x) {
+    if (size == arr.length - 1) {
+      enlargeArray(arr.length * 2 + 1);
+    }
+
+    // percolate up
+    int hole = ++size;
+    arr[0] = x;
+    while (arr[hole/2].compareTo(x) > 0) {
+      arr[hole] = arr[hole/2]; // move parent down and hole up
+      hole /= 2;
+    }
+    arr[hole] = x;
+  }
+
+  private void enlargeArray(int newSize) {
+    T[] old = arr;
+    arr = (T[]) new Comparable[newSize + 1];
+    for (int i = 0; i < old.length; i++) {
+      arr[i] = old[i];
+    }
   }
 
   public T findMin() {
-    return null;
+    if (arr.length == 0) {
+      throw new java.util.NoSuchElementException();
+    }
+    return arr[1];
   }
 
   public T deleteMin() {
@@ -40,5 +68,11 @@ public class BinaryHeap <T extends Comparable<? super T>> {
   }
 
   public static void main(String[] args) {
+    BinaryHeap<Integer> bh = new BinaryHeap<>();
+    bh.insert(30);
+    bh.insert(10);
+    System.out.println(bh.findMin());
+    bh.insert(-1);
+    System.out.println(bh.findMin());
   }
 }
